dfa3278917dc93970a3066a0f37b920867eae0a8
johnhtml <jogerenag@unal.edu.co>
2022-07-04 20:13:02 -0500

    spring boot

---
+++ src/main/java/com/example/paginationApplication/PaginationApplication.java
@@ -0,0 +1,16 @@
+package com.example.paginationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationApplication.class, args);
+	}
+
+	//This project was bootstrapped at--
+	//https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.7.1&packaging=jar&jvmVersion=18&groupId=com.example&artifactId=paginationApplication&name=paginationApplication&description=Server%20coded%20in%20Spring%20boot%2C%20this%20server%20allow%20us%20to%20paginate%20large%20amount%20of%20data%20&packageName=com.example.paginationApplication&dependencies=web,mysql,data-jpa,lombok
+
+}
